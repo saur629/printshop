038e9de2ceb78af69d84aa84b11fb665798599ec
saur629 <saurabhyd2004@gmail.com>
2026-03-28 16:34:45 +0530

    Fix: update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true,
-})
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      { protocol: 'https', hostname: 'utfs.io' },
+      { protocol: 'https', hostname: 'uploadthing.com' },
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+    ],
+  },
 }
 
-module.exports = withPWA(nextConfig)
+module.exports = nextConfig
